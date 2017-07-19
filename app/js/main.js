a5f80396e7af5631efdc94fd5e649d97e527eed4
marseille78 <boyko.ruslan78@gmail.com>
2017-07-20 00:44:46 +0300

    Slider start

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,59 @@
+/*Add navigation (in file maim.js)*/
+// var nav = new Nav({ el: '.nav' });
+
+// nav.toggle.addEventListener('click', function(e) {
+// 	nav.toggleMenu(e);
+// })
+
+function Nav(obj) {
+	this.el = document.querySelector(obj.el);
+	this.toggle = (obj.toggle) ? document.querySelector(obj.toggle) : document.querySelector(obj.el + ' .js-toggleMenu');
+	this.wrapper = (obj.wrapper) ? document.querySelector(obj.wrapper) : document.querySelector(obj.el + ' .nav__wrapper');
+}
+
+Nav.prototype.toggleMenu = function(e) {
+	e.preventDefault();
+	this.wrapper.classList.toggle('opened');
+	document.body.classList.toggle('opened');
+}
+/***********************************
+ * Slider
+ ***********************************/
+var slideIndex = 1;
+
+showSlides(slideIndex);
+
+function plusSlides(n) {
+	showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+	showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+	var i;
+	var slides = document.querySelectorAll('.slider__item');
+	var dots = document.querySelectorAll('.slider__dot');
+
+	if (n > slides.length) {
+		slideIndex = 1;
+	}
+
+	if (n < 1) {
+		slideIndex = slides.length;
+	}
+
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = 'none';
+	}
+
+	for (i = 0; i < dots.length; i++) {
+		dots[i].className = dots[i].className.replace('active', '');
+	}
+
+	slides[slideIndex - 1].style.display = 'block';
+}
 /**
  * Обробка при клиці на layout
  * --------------------------------
@@ -27,22 +83,4 @@ function removeAllOpened() {
 	for (var i = 0; i < allOpened.length; i++) {
 		allOpened[i].classList.remove('opened');
 	}
-}
-/*Add navigation (in file maim.js)*/
-// var nav = new Nav({ el: '.nav' });
-
-// nav.toggle.addEventListener('click', function(e) {
-// 	nav.toggleMenu(e);
-// })
-
-function Nav(obj) {
-	this.el = document.querySelector(obj.el);
-	this.toggle = (obj.toggle) ? document.querySelector(obj.toggle) : document.querySelector(obj.el + ' .js-toggleMenu');
-	this.wrapper = (obj.wrapper) ? document.querySelector(obj.wrapper) : document.querySelector(obj.el + ' .nav__wrapper');
-}
-
-Nav.prototype.toggleMenu = function(e) {
-	e.preventDefault();
-	this.wrapper.classList.toggle('opened');
-	document.body.classList.toggle('opened');
 }
