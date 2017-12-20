2737e55fd7af170964a65b32604182872b3dcb69
marseille78 <boyko.ruslan78@gmail.com>
2017-12-20 13:20:46 +0200

    2017.12.20: Fix main-menu

--- components/main-menu/main-menu.js
+++ components/main-menu/main-menu.js
@@ -1,7 +1,12 @@
+/**
+ * Объект главного меню
+ * @param el {String} - Селектор элемента
+ * @constructor
+ */
 function MainMenu(el) {
     this.el = document.querySelector(el);
     this.btnToggle = this.el.querySelector('.js-mainMenuToggle');
-    this.list = this.el.querySelector('.main-menu__list');
+    this.list = this.el.querySelector(el + '__list');
 
     var clearActive = this.clearActive.bind(this);
     var toggleMenu = this.toggleMenu.bind(this);
@@ -9,10 +14,18 @@ function MainMenu(el) {
     this.btnToggle.addEventListener('click', toggleMenu);
     window.addEventListener('resize', clearActive);
 }
+
+/**
+ * Открытие/Закрытие мобильного меню
+ */
 MainMenu.prototype.toggleMenu = function() {
     this.btnToggle.classList.toggle('active');
     this.list.classList.toggle('active');
 };
+
+/**
+ * Сброс класса .active (Закрытие мобильного меню) при изменении окна браузера
+ */
 MainMenu.prototype.clearActive = function() {
     this.btnToggle.classList.remove('active');
     this.list.classList.remove('active');
